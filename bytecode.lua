01c2fb5dc9b888b4e46860e1171bd96f2b6d0810
Francesco Abbate <francesco.bbt@gmail.com>
2013-11-03 16:56:57 +0100

    Simplify code in proto's bytecode generator

--- bytecode.lua
+++ bytecode.lua
@@ -556,6 +556,17 @@ function Proto.__index:here(name)
    end
    return name
 end
+function Proto.__index:enable_jump(name)
+   if type(name) == 'number' then
+      error("bad label")
+   end
+   local here = self.tohere[name]
+   if not here then
+      here = { }
+      self.tohere[name] = here
+   end
+   here[#here + 1] = #self.code + 1
+end
 function Proto.__index:jump(name)
    if self.labels[name] then
       -- backward jump
@@ -568,15 +579,7 @@ function Proto.__index:jump(name)
       end
    else
       -- forward jump
-      if type(name) == 'number' then
-         error("bad label")
-      end
-      local here = self.tohere[name]
-      if not here then
-         here = { }
-         self.tohere[name] = here
-      end
-      here[#here + 1] = #self.code + 1
+      self:enable_jump(name)
       return self:emit(BC.JMP, self.freereg, NO_JMP)
    end
 end
@@ -587,12 +590,7 @@ function Proto.__index:loop(name)
       return self:emit(BC.LOOP, self.freereg, offs - #self.code)
    else
       -- forward jump
-      local here = self.tohere[name]
-      if not here then
-         here = { }
-         self.tohere[name] = here
-      end
-      here[#here + 1] = #self.code + 1
+      self:enable_jump(name)
       return self:emit(BC.LOOP, self.freereg, NO_JMP)
    end
 end
