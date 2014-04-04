b748fa60458a95f48ba1907fd7d9ac4bb0ced6c8
Richard Hundt <richardhundt@gmail.com>
2014-04-04 12:13:14 +0200

    re-instate short lambda expression

--- src/lang/parser.lua
+++ src/lang/parser.lua
@@ -274,7 +274,9 @@ local patt = [=[
 
    func_expr <- (
       "function" <idsafe> s <func_head> s <func_body>
-      / (<func_head> / {| |}) s "=>" s <func_body>
+      / (<func_head> / {| |}) s "=>" (
+         hs <expr> / s <block_stmt> s (<end> / %1 => error) / %1 => error
+      )
    ) -> funcExpr
 
    func_body <- <block_stmt> s (<end> / %1 => error)
